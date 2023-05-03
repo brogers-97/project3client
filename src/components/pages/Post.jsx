9e48f2b3061ba7d97dbd3da4fa55e9addedfe1cc
brogers-97 <brburr24@gmail.com>
2023-05-03 11:13:57 -0700

    added components and routes (skeleton), removed the api-v1 in api requests

---
+++ src/components/pages/Post.jsx
@@ -0,0 +1,10 @@
+export default function Post() {
+
+    const border = {border: '1px solid black'}
+
+    return(
+        <div style={border}>
+            <h2>I am a post component</h2>
+        </div>
+    )
+}
