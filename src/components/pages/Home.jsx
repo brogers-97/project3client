4c3c401e4c5545363acc25ae3b0b9bbe1e2af6c3
jaronw1 <jaronwong6@gmail.com>
2023-05-05 09:45:18 -0700

    rendered Posts for the homepage

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,38 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Post from '../partials/Post';
+
 export default function Home() {
-    return (
-        <div>
-            <h1>Home page</h1>
-        </div>
-    )
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/posts`)
+      .then((response) => {
+        const postData = response.data;
+        setPosts(postData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+
+
+  return (
+    <div>
+      <h1>Home page</h1>
+
+        {posts.map((post) => (
+          <div key={post.id}>
+            <a href={`/posts/${post._id}`}>
+                <Post 
+                    post={post}
+                />
+            </a>
+          </div>
+        ))}
+
+    </div>
+  );
 }
