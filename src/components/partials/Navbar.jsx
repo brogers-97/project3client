b74c26d7cb161af043dcba5fde6ccb115c749eb6
Andrew <andrew@andrewsmith.org>
2023-05-07 03:08:29 -0400

    Implement dropdown menus on desktop and mobile
    Realized we'd need to break out the dropdown menu into its own component to allow it to appear below the navbar.  Haven't gotten that working yet but went ahead and broke it out in anticipation of that work.

--- src/components/partials/Navbar.jsx
+++ src/components/partials/Navbar.jsx
@@ -2,25 +2,26 @@ import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import '../../navbar.css'
+import Dropdown from './Dropdown'
 
 export default function Navbar({ currentUser, handleLogout }) {
     const navigate = useNavigate()
     const [value, setValue] = useState('')
-
-    const handleChange = (e) => {
-        const location = e.target.value
-        setValue(location)
-        navigate(location)
-    }
+    const [menuOpen, setMenuOpen] = useState(false)
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     const loggedInNavbarItems = (
         <>
+            {/* Responsive button -- appears on small screens */}
             <button
                 data-collapse-toggle="navbar-dropdown"
+                onClick={() => {
+                    setMobileMenuOpen(!mobileMenuOpen)
+                }}
                 type="button"
                 class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 aria-controls="navbar-dropdown"
-                aria-expanded="false"
+                aria-expanded="true"
             >
                 <span class="sr-only">Open main menu</span>
                 <svg
@@ -37,7 +38,14 @@ export default function Navbar({ currentUser, handleLogout }) {
                     ></path>
                 </svg>
             </button>
-            <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+            {/* this div allows the menu to be hidden on mobile
+			devices if hamburger button hasn't been clicked */}
+            <div
+                className={`${
+                    mobileMenuOpen ? null : `hidden`
+                } w-full md:block md:w-auto`}
+                id="navbar-dropdown"
+            >
                 <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
                         <a
@@ -50,8 +58,9 @@ export default function Navbar({ currentUser, handleLogout }) {
                     </li>
                     <li>
                         <button
-                            id="dropdownNavbarLink"
-                            data-dropdown-toggle="dropdownNavbar"
+                            onClick={() => {
+                                setMenuOpen(!menuOpen)
+                            }}
                             class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                         >
                             Make a post{' '}
@@ -69,49 +78,14 @@ export default function Navbar({ currentUser, handleLogout }) {
                                 ></path>
                             </svg>
                         </button>
-                        {/* <!-- Dropdown menu --> */}
-                        <div
-                            id="dropdownNavbar"
-                            class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
-                        >
-                            <ul
-                                class="py-2 text-sm text-gray-700 dark:text-gray-400"
-                                aria-labelledby="dropdownLargeButton"
-                            >
-                                <li>
-                                    <a
-                                        href="#"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                    >
-                                        Dashboard
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                    >
-                                        Settings
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="#"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                    >
-                                        Earnings
-                                    </a>
-                                </li>
-                            </ul>
-                            <div class="py-1">
-                                <a
-                                    href="#"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
-                                >
-                                    Sign out
-                                </a>
-                            </div>
-                        </div>
+                        {menuOpen ? (
+                            <Dropdown
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                            />
+                        ) : null}
                     </li>
                     <li>
                         <a
@@ -169,55 +143,6 @@ export default function Navbar({ currentUser, handleLogout }) {
 
     const navbarFrame = (
         <>
-            {/* if the user is logged in... */}
-            {/* <nav className="nav-bar">
-                <div className="nav-left">
-                    <div className="btn">
-                        <Link to="/home">
-                            <p>Home</p>
-                        </Link>
-                    </div>
-
-                    <div className="btn">
-                        <Link to="/">
-                            <p>Friends</p>
-                        </Link>
-                    </div>
-
-                    <div className="btn">
-                        <select
-                            onChange={handleChange}
-                            value={value}
-                            name="create-post"
-                        >
-                            <option value={'/new'}>New Post</option>
-
-                            <option value={'/search'}>New review</option>
-                        </select>
-                    </div>
-                </div>
-
-				<div className='nav-right'>
-				<div className='btn'>
-						<Link to="/search">
-							<p>Search</p>
-						</Link>
-					</div>
-					
-					<div className='btn'>
-						<Link to="/profile">
-							<p>profile</p>
-						</Link>
-					</div>
-
-                    <div className="btn">
-                        <Link to="/">
-                            <p onClick={handleLogout}>logout</p>
-                        </Link>
-                    </div>
-                </div>
-            </nav> */}
-
             <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
                 <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <div class="flex items-center">
