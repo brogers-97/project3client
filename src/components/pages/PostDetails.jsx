666d25b1cd6ff5e848c5af2b99e9bc058ea838d1
Andrew <andrew@andrewsmith.org>
2023-05-09 15:14:25 -0400

    Implement button for deleting posts

--- src/components/pages/PostDetails.jsx
+++ src/components/pages/PostDetails.jsx
@@ -10,7 +10,7 @@ import Comments from '../partials/Comments'
 
 export default function PostDetails({ currentUser, setCurrentUser }) {
     const [post, setPost] = useState({})
-    const [author, setAuthor] = useState("")
+    const [author, setAuthor] = useState('')
     const [postLoaded, setPostLoaded] = useState(false)
     const [showForm, setShowForm] = useState(false)
 
@@ -54,6 +54,25 @@ export default function PostDetails({ currentUser, setCurrentUser }) {
         }
     }
 
+    const handleDelete = async () => {
+        try {
+            const token = localStorage.getItem('jwt')
+            // make the auth headers
+            const options = {
+                headers: {
+                    Authorization: token,
+                },
+            }
+            await axios.delete(
+                `${process.env.REACT_APP_SERVER_URL}/posts/${id}`,
+                options
+            )
+            navigate('/home')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     // const handleDeleteClick = async () => {
     //     try {
     //         await axios.delete(
@@ -88,10 +107,18 @@ export default function PostDetails({ currentUser, setCurrentUser }) {
                     {/* first layer of ternary checks whether there's a user loaded in state.  once the current user is loaded in state, second layer of ternary checks whether the logged in user's name matches that of the author.  if so, client shows edit button. 
                     
                     with more time, ideally we'd do server side checking to confirm that the logged in user matches the user who wrote the post, as this implementation is susceptible to a user changing state in dev tools to cause the edit button to appear on someone else's post */}
-                    {currentUser ? 
-                    (currentUser.name === author ?
-                    <button onClick={() => setShowForm(true)}>Edit</button>
-                    : null) : null}
+                    {currentUser ? (
+                        currentUser.name === author ? (
+                            <>
+                                <button onClick={() => setShowForm(true)}>
+                                    Edit
+                                </button>
+                                <button onClick={() => handleDelete()}>
+                                    Delete
+                                </button>
+                            </>
+                        ) : null
+                    ) : null}
                     <Comments
                         currentUser={currentUser}
                         id={id}
